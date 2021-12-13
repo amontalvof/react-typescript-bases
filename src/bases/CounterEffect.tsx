c8b82551449f108e4781181080bbf7ded6c9d294
amontalvof <amontalvof92@gmail.com>
2021-12-12 20:09:04 -0600

    feat: Perform animation when maximum value is reached

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const MAXIMUM_COUNT = 10;
 
@@ -17,11 +18,15 @@ const CounterEffect = () => {
             '%cThe maximum value was reached',
             'color: red; background-color: black'
         );
+        gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() =>
+            gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' })
+        );
     }, [counter]);
 
     return (
         <>
-            <h1>CounterEffect: {counter}</h1>
+            <h1>CounterEffect: </h1>
+            <h2>{counter}</h2>
             <button onClick={handleClick}>+1</button>
         </>
     );
